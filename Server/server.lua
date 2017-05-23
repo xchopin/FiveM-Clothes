98418be12f1d1e50d6349e39ce4e71221175518c
Xavier Chopin <xchopin54@gmail.com>
2017-05-23 18:48:11 +0200

    Changed Spawn Event

--- Server/server.lua
+++ Server/server.lua
@@ -1,5 +1,37 @@
 require "resources/essentialmode/lib/MySQL"
-MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "root")
+MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "PASSWORD")
+
+
+SQL_COLUMNS = {
+	'skin',
+	'face',							
+	'face_texture',
+	'hair',
+	'hair_texture',
+	'shirt',
+	'shirt_texture',
+	'pants',
+	'pants_texture',
+	'shoes',
+	'shoes_texture',
+	'vest',
+	'vest_texture',
+	'bag',
+	'bag_texture',
+	'hat',
+	'hat_texture',
+	'mask',
+	'mask_texture',
+	'glasses',
+	'glasses_texture',
+	'gloves',
+	'gloves_texture',
+	'jacket',
+	'jacket_texture',
+	'ears',
+	'ears_texture'
+}
+
 
 RegisterServerEvent("clothing_shop:SpawnPlayer_server")
 AddEventHandler("clothing_shop:SpawnPlayer_server", function()
@@ -10,11 +42,11 @@ AddEventHandler("clothing_shop:SpawnPlayer_server", function()
         if(result[1].isFirstConnection == 1)then
             MySQL:executeQuery("INSERT INTO user_clothes(identifier) VALUES ('@identifier')",{['@identifier']=player})
             MySQL:executeQuery("UPDATE users SET isFirstConnection = 0 WHERE identifier = '@username'", {['@username'] = player})
-            TriggerClientEvent("clothing_shop:loadSkin_client", source, "mp_m_freemode_01") -- Updates the client's skin
+            TriggerClientEvent("clothing_shop:loadItems_client", source, nil) -- Updates the client's skin with default values
 		else
-            executed_query = MySQL:executeQuery("SELECT skin FROM outfits WHERE identifier = '@username'", {['@username'] = player})
-		    local result2 = MySQL:getResults(executed_query, {'skin'}, "identifier")
-			TriggerClientEvent("clothing_shop:loadSkin_client", source, result2[1].skin)
+            executed_query = MySQL:executeQuery("SELECT * FROM user_clothes WHERE identifier = '@username'", {['@username'] = player})
+		    result = MySQL:getResults(executed_query, SQL_COLUMNS, "identifier")
+			TriggerClientEvent("clothing_shop:loadItems_client", source, result[1])
         end
 	end)
 end)
